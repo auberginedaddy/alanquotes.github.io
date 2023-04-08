e2524c069a75dd946b71dbeb73142db2c40c0082
ThatOnePerson <87896375+ThatOnePers0n@users.noreply.github.com>
2023-04-07 23:25:02 -0700

    Update splash.js

--- splash.js
+++ splash.js
@@ -15,5 +15,6 @@ splash[13] = "It's gay now";
 splash[14] = "I'm gonna die now";
 splash[15] = "I'm going to tbag your body";
 splash[16] = "Send ball pics now";
+splash[17] = "Go back to dying";
 var i = Math.floor(16*Math.random());
 text_location.innerHTML = splash[i];
